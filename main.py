8127c9e0c99c86b4657e6c09528496c6d323cb02
sid.chak <65286878+Siddharth-Chakma@users.noreply.github.com>
2022-03-19 17:19:13 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import glob
+import numpy as np
+import cv2
+
+
+def sp_noise(image, prob):
+    '''
+    Add salt and pepper noise to image
+    prob: Probability of the noise
+    '''
+    output = image.copy()
+    if len(image.shape) == 2:
+        black = 0
+        white = 255
+    else:
+        colorspace = image.shape[2]
+        if colorspace == 3:  # RGB
+            black = np.array([0, 0, 0], dtype='uint8')
+            white = np.array([255, 255, 255], dtype='uint8')
+        else:  # RGBA
+            black = np.array([0, 0, 0, 255], dtype='uint8')
+            white = np.array([255, 255, 255, 255], dtype='uint8')
+    probs = np.random.random(output.shape[:2])
+    output[probs < (prob / 2)] = black
+    output[probs > 1 - (prob / 2)] = white
+    return output
+
+
+path = "test_images/imgs/*.*"
+for file in glob.glob(path):
+
+        print(file)
+        img = cv2.imread(file,1)
+        out=sp_noise(img,.01)
+        cv2.imwrite(file,out)
